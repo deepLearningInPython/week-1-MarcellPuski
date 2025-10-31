0d6cf92d4822b2e8d9697b18926d018136818510
MarcellPuski <marcellpuski@gmail.com>
2025-10-31 17:38:22 +0100

    solving task 1

--- tasks.py
+++ tasks.py
@@ -12,8 +12,14 @@ import numpy as np
 
 # Your code here:
 # ----------------------------------------------
-def step ()
+def step(x):
+    if x > 0:
+        return 1
+    else:
+        return -1
 
+
+print(step(5))
 
 # -----------------------------------------------
 
